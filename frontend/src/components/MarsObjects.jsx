b18a5d4933b72ba720011823c2b7b013b6c5ee9f
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-30 01:10:21 +0200

    More info small fix

--- frontend/src/components/MarsObjects.jsx
+++ frontend/src/components/MarsObjects.jsx
@@ -7,6 +7,7 @@ function MarsObjects({objectsOnMars}) {
     <div className="marsObjects">
       <h2>Artificial objects on Mars</h2>
       {console.log(objectsOnMars)}
+      {/* filter radio button here */}
       {objectsOnMars.map((obj) => (<MarsObject key={obj.object} object={obj}/>))}
     </div>
   )
