944a0d51b1bd2ea1579b62e6a112ce508219b202
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-30 00:21:59 +0200

    Toggle showMoreBtn text

--- frontend/src/components/MarsObject.jsx
+++ frontend/src/components/MarsObject.jsx
@@ -3,15 +3,17 @@ import React, { useState } from 'react'
 function MarsObject({object}) {
 
   const [showMore, setShowMore] = useState(false);
+  const [more, setMore] = useState("+");
 
   function moreInfo(){
     setShowMore(!showMore);
+    more==="+" ? setMore("-") : setMore("+");
   }
 
   return (
     <div>
       {object.object}
-      <button className="showMoreBtn" onClick={moreInfo}>+</button>
+      <button className="showMoreBtn" onClick={moreInfo}>{more}</button>
       {showMore && 
       <div className="moreInfo">
         <p>year: {object.year}</p> 
