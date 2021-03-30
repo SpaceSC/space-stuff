dfe822a00cb0284ee3314642f157684e81cd0c83
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-30 11:45:11 +0200

    Align radio buttons and filter results

--- frontend/src/components/MarsObjects.jsx
+++ frontend/src/components/MarsObjects.jsx
@@ -37,28 +37,27 @@ function MarsObjects({objectsOnMars}) {
     <div className="marsObjects">
       <h2>Artificial objects on Mars</h2>
       
-      {objectsOnMars.filter(checkObjectType).filter(checkObjectOperation).map(obj => (<MarsObject key={obj.object} object={obj}/>))}
-      {/*
-      typeSelected==="all" && !operationSelected==="all" ? objectsOnMars.filter(checkObjectOperation).map((obj) => <MarsObject key={obj.object} object={obj}/>) :
-  !typeSelected==="all" && operationSelected==="all" ? */}
-      
+      <div className="radios">
+        <div className="radioTop">
+          <input type="radio" id="all" name="selectType" value="all" onChange={setObjectType} defaultChecked/>
+          <label htmlFor="all">all</label>
+          <input type="radio" id="rovers" name="selectType" value="rover" onChange={setObjectType}/>
+          <label htmlFor="rovers">rovers</label>
+          <input type="radio" id="helicopter" name="selectType" value="helicopter" onChange={setObjectType}/>
+          <label htmlFor="helicopter">helicopter</label>
+        </div>
+        
+        <div className="radioBottom">
+          <input type="radio" id="all" name="selectOperation" value="all" onChange={setObjectOperation} defaultChecked/>
+          <label htmlFor="all">all</label>
+          <input type="radio" id="inOperation" name="selectOperation" value="true" onChange={setObjectOperation}/>
+          <label htmlFor="inOperation">in operation</label>
+          <input type="radio" id="notInOperation" name="selectOperation" value="false" onChange={setObjectOperation}/>
+          <label htmlFor="notInOperation">not in operation</label>
+        </div>
+      </div>
 
-      <div className="radios">
-        <input type="radio" id="all" name="selectType" value="all" onChange={setObjectType} defaultChecked/>
-        <label htmlFor="all">all</label>
-        <input type="radio" id="rovers" name="selectType" value="rover" onChange={setObjectType}/>
-        <label htmlFor="rovers">rovers</label>
-        <input type="radio" id="helicopter" name="selectType" value="helicopter" onChange={setObjectType}/>
-        <label htmlFor="helicopter">helicopter</label>
-      </div>
-      <div className="radios">
-        <input type="radio" id="all" name="selectOperation" value="all" onChange={setObjectOperation} defaultChecked/>
-        <label htmlFor="all">all</label>
-        <input type="radio" id="inOperation" name="selectOperation" value="true" onChange={setObjectOperation}/>
-        <label htmlFor="inOperation">in operation</label>
-        <input type="radio" id="notInOperation" name="selectOperation" value="false" onChange={setObjectOperation}/>
-        <label htmlFor="notInOperation">not in operation</label>
-      </div>
+      <div className="filtered">{objectsOnMars.filter(checkObjectType).filter(checkObjectOperation).map(obj => (<MarsObject key={obj.object} object={obj}/>))}</div>
     </div>
   )
 }
