cbf9ee9cc667e0c9c653030c34d9c663c5b0a301
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-30 09:10:05 +0200

    Add radio buttons to select type

--- frontend/src/components/MarsObjects.jsx
+++ frontend/src/components/MarsObjects.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MarsObject from './MarsObject'
 
 function MarsObjects({objectsOnMars}) {
+
+  const [type, setType] = useState("all");
+
+  function setObjectType(e){
+    setType(e.target.value);
+  }
 
   return (
     <div className="marsObjects">
       <h2>Artificial objects on Mars</h2>
       {console.log(objectsOnMars)}
-      {/* filter radio button here */}
       {objectsOnMars.map((obj) => (<MarsObject key={obj.object} object={obj}/>))}
+      <div className="radios">
+        <input type="radio" id="all" name="selectType" value="all" onChange={setObjectType} checked="checked"/>
+        <label for="all">all</label>
+        <input type="radio" id="rovers" name="selectType" value="rover" onChange={setObjectType}/>
+        <label for="rovers">rovers</label>
+        <input type="radio" id="helicopter" name="selectType" value="helicopter" onChange={setObjectType}/>
+        <label for="helicopter">helicopter</label>
+      </div>
     </div>
   )
 }
