d6f2223d41caabb4b94cef7137d9b3fda7c9e286
SpaceSC <ceciliasandor.22@gmail.com>
2021-04-23 18:50:17 +0200

    Add comments to index.js in backend

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 const cors = require('cors');
 
+// Load the cors middleware
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.get('/', (req, res) => {
@@ -10,6 +11,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/data/objects.json')
 })
 
+// Listen on port 5000
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
